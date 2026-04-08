619813683e16cb39d2e7d08579aed3af156d9073
Nguyen Long <lucky4126666@gmail.com>
2026-04-08 13:54:17 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,16 @@
+import sqlite3
+
+DB = "data.db"
+
+
+def get_conn():
+    return sqlite3.connect(DB)
+
+
 def init_db():
     conn = get_conn()
     cur = conn.cursor()
 
-    # memory
     cur.execute("""
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,7 +20,6 @@ def init_db():
     )
     """)
 
-    # settings
     cur.execute("""
     CREATE TABLE IF NOT EXISTS settings (
         key TEXT PRIMARY KEY,
@@ -24,26 +31,22 @@ def init_db():
     conn.close()
 
 
-def get_setting(key):
-    conn = get_conn()
-    cur = conn.cursor()
-
-    cur.execute("SELECT value FROM settings WHERE key=?", (key,))
-    row = cur.fetchone()
-    conn.close()
-
-    return row[0] if row else None
-
-
 def set_setting(key, value):
     conn = get_conn()
     cur = conn.cursor()
-
     cur.execute("""
     INSERT INTO settings(key,value)
     VALUES (?,?)
     ON CONFLICT(key) DO UPDATE SET value=excluded.value
     """, (key, value))
-
     conn.commit()
     conn.close()
+
+
+def get_setting(key):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT value FROM settings WHERE key=?", (key,))
+    row = cur.fetchone()
+    conn.close()
+    return row[0] if row else None
