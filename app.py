f12af4e4f4446311c7938eab6f622ec7f1c68d29
Nguyen Long <lucky4126666@gmail.com>
2026-04-08 10:28:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -155,5 +155,10 @@ def home():
     return {"status": "webhook running"}
 
 # ===== RUN =====
+import os
+
+PORT = int(os.getenv("PORT", 8080))
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=PORT)
