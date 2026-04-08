e6f450a7a77199bafe03957dab4a5270a19730a0
Nguyen Long <lucky4126666@gmail.com>
2026-04-08 10:23:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
+BASE_URL = os.getenv("BASE_URL")
 PORT = int(os.getenv("PORT", 8080))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
 if not BOT_TOKEN:
-    raise Exception("❌ BOT_TOKEN missing")
+    raise Exception("Missing BOT_TOKEN")
 
-if not DATABASE_URL:
-    raise Exception("❌ DATABASE_URL missing")
+if not BASE_URL:
+    raise Exception("Missing BASE_URL")
