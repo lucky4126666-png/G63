f8d679df38175ebb267c149d5bc1d8eaa0a6dd17
Nguyen Long <lucky4126666@gmail.com>
2026-04-08 13:33:15 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,16 +1,8 @@
-import sqlite3
-
-DB_NAME = "data.db"
-
-
-def get_conn():
-    return sqlite3.connect(DB_NAME)
-
-
 def init_db():
     conn = get_conn()
     cur = conn.cursor()
 
+    # memory
     cur.execute("""
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,6 +11,39 @@ def init_db():
         content TEXT
     )
     """)
+
+    # settings
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS settings (
+        key TEXT PRIMARY KEY,
+        value TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def get_setting(key):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("SELECT value FROM settings WHERE key=?", (key,))
+    row = cur.fetchone()
+    conn.close()
+
+    return row[0] if row else None
+
+
+def set_setting(key, value):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT INTO settings(key,value)
+    VALUES (?,?)
+    ON CONFLICT(key) DO UPDATE SET value=excluded.value
+    """, (key, value))
 
     conn.commit()
     conn.close()
