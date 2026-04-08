8afd9157b1eee0107d35fc5ca5d3c7799cd154af
Nguyen Long <lucky4126666@gmail.com>
2026-04-08 13:04:17 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,13 +1,17 @@
 import sqlite3
 
+DB_NAME = "data.db"
+
+
 def get_conn():
-    return sqlite3.connect("data.db")
+    return sqlite3.connect(DB_NAME)
+
 
 def init_db():
     conn = get_conn()
-    c = conn.cursor()
+    cur = conn.cursor()
 
-    c.execute("""
+    cur.execute("""
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
