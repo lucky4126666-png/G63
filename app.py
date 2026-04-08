46295e38537b71fb709d71522132cb13023669e5
Nguyen Long <lucky4126666@gmail.com>
2026-04-08 13:27:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,14 +30,13 @@ async def start(msg: types.Message):
     if msg.chat.type != "private":
         return
 
-   text = """点击此处可以添加机器人进群
+    text = """点击此处可以添加机器人进群
 http://t.me/xbqgk?startgroup=foo
 
 更多服务，请访问 https://t.me/xbkf/
 """
 
-
-    await msg.answer(text, disable_web_page_preview=True)
+    await msg.answer(text)
 # ===== AI HANDLER =====
 @dp.message()
 async def handle_message(msg: types.Message):
